a8102e064e5927263929acbdde2fcd670c40859c
mahvash-fatima <mahvash.sayed@gmail.com>
2017-08-10 11:57:44 +0530

    Install Sass and remove all previous css from style.css file

--- php/class-plugin.php
+++ php/class-plugin.php
@@ -56,7 +56,7 @@ class Plugin extends Plugin_Base {
 		global $post;
 
 		if ( Tackle_Post_Type::SLUG === $post->post_type ) {
-			wp_register_style( 'tackle-styles', plugin_dir_url( __DIR__ ) . 'css/style.css' );
+			wp_register_style( 'tackle-styles', plugin_dir_url( __DIR__ ) . 'css/sass/style.css' );
 			wp_register_style( 'tackle-font-awesome', plugin_dir_url( __DIR__ ) . 'css/vendor/font-awesome/css/font-awesome.min.css' );
 			wp_enqueue_style( 'tackle-styles' );
 			wp_enqueue_style( 'tackle-font-awesome' );
