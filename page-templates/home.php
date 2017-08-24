7f8b8dfff923aef25c1b1c3a98a73cdd08c8ea98
mahvash-fatima <mahvash.sayed@gmail.com>
2017-08-24 11:56:54 +0530

    Add style in home page and install slick slider

--- page-templates/home.php
+++ page-templates/home.php
@@ -15,12 +15,14 @@ tackle_get_header();
 
 			<!--Section 1a-->
 			<div class="tackle-home-template-section-1a-primary large-6 medium-6 cell">
-				<h2 class="tackle-home-template-title">Welcome to Tackle, Mahvash!</h2>
-				<p class="tackle-home-template-paragraph">Here's how to get started:</p>
+				<div class="tackle-home-temeplate-header margin-bottom">
+					<h2 class="tackle-home-template-title">Welcome to Tackle, Mahvash!</h2>
+					<p class="tackle-home-template-paragraph">Here's how to get started:</p>
+				</div>
 
 				<ul class="tackle-home-template-get-started-list">
-					<li><a href=""><i class="fa fa-play-circle-o" aria-hidden="true"></i>Learn the basics of time tracking</a></li>
-					<li><a href="<?php echo home_url() . '/tackle/new-project'; ?>"><i class="fa fa-file-text-o" aria-hidden="true"></i>Create a project</a></li>
+					<li><a href=""><i class="fa fa-play-circle" aria-hidden="true"></i>Learn the basics of time tracking</a></li>
+					<li><a href="<?php echo home_url() . '/tackle/new-project'; ?>"><i class="fa fa-file-text" aria-hidden="true"></i>Create a project</a></li>
 					<li><a href=""><i class="fa fa-clock-o" aria-hidden="true"></i>Track your first hour</a></li>
 					<li><a href=""><i class="fa fa-mobile" aria-hidden="true"></i>Get the apps and browser extension</a></li>
 				</ul>
--- php/class-plugin.php
+++ php/class-plugin.php
@@ -47,11 +47,13 @@ class Plugin extends Plugin_Base {
 			wp_register_script( 'tackle-what-input', plugin_dir_url( __DIR__ ) . 'js/vendor/foundation/what-input.js', array( 'jquery' ), 0.1 );
 			wp_register_script( 'tackle-foundation', plugin_dir_url( __DIR__ ) . 'js/vendor/foundation/foundation.js', array( 'jquery' ), 0.1 );
 			wp_register_script( 'tackle-foundation-app', plugin_dir_url( __DIR__ ) . 'js/vendor/foundation/app.js', array( 'jquery' ), 0.1 );
+			wp_register_script( 'tackle-slick-script', plugin_dir_url( __DIR__ ) . 'js/vendor/slick/slick.min.js', array( 'jquery' ), null, true );
 			wp_enqueue_script( 'tackle-main', 20, 1 );
 			wp_enqueue_script( 'tackle-foundation-jquery' );
 			wp_enqueue_script( 'tackle-what-input' );
 			wp_enqueue_script( 'tackle-foundation' );
 			wp_enqueue_script( 'tackle-foundation-app' );
+			wp_enqueue_script( 'tackle-slick-script', 21, 1 );
 		}
 	}
 
@@ -67,11 +69,15 @@ class Plugin extends Plugin_Base {
 			wp_register_style( 'tackle-styles', plugin_dir_url( __DIR__ ) . 'css/sass/style.css' );
 			wp_register_style( 'tackle-font-awesome', plugin_dir_url( __DIR__ ) . 'css/vendor/font-awesome/css/font-awesome.min.css' );
 			wp_register_style( 'tackle-foundation', plugin_dir_url( __DIR__ ) . 'css/vendor/foundation/foundation.css' );
+			wp_register_style( 'tackle-slick-styles', plugin_dir_url( __DIR__ ) . 'css/vendor/slick/slick.css' );
+			wp_register_style( 'tackle-slick-theme-styles', plugin_dir_url( __DIR__ ) . 'css/vendor/slick/slick-theme.css' );
 			wp_enqueue_style( 'tackle-styles' );
 			wp_enqueue_style( 'tackle-font-awesome' );
 			wp_enqueue_style( 'tackle-foundation' );
 			wp_register_style( 'tackle-fonts', tackle_fonts_url(), array(), null );
 			wp_enqueue_style( 'tackle-fonts' );
+			wp_enqueue_style( 'tackle-slick-styles' );
+			wp_enqueue_style( 'tackle-slick-theme-styles' );
 		}
 	}
 }
